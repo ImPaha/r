6c0c58df8fa4efef67818e1dc576ffc5a194dd49
Dimexz <dmnoykin@gmail.com>
2020-02-10 20:08:56 +0700

    Initial commit

---
+++ src/4t65.kt
@@ -0,0 +1,39 @@
+import java.util.*
+
+
+
+
+class Pupils{
+var indetification :Array<String>=
+    var cclass = Grooop()
+    var nsm :Array<Indetification> = arrayOf("Pasha")
+
+}
+class Grooop{
+
+    var spclass = Kvant()
+    var biof : Array<Pupils> = arrayOf()
+
+}
+
+
+
+
+
+class Kvant{
+    var exclass = Kvantorium()
+var bio : Array<Grooop>
+
+}
+class Kvantorium{
+    var kvant = Kvant()
+    var bio = kvant.bio
+     var kvantums: Array<Kvant> = arrayOf(bio, )
+
+ }
+
+
+
+fun main(){
+
+}
